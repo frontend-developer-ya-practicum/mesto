8c577b31a445ed636f3be195d750f082d2e2a503
Александр Морев <a.morev@kryptonite.ru>
2022-05-29 16:51:52 +0300

    feat: config hover buttons

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,16 @@ let edit_profile_save_button = popup.querySelector(".popup__save");
 let edit_profile_name = popup.querySelector(".popup__input-name");
 let edit_profile_about = popup.querySelector(".popup__input-about");
 
+let card_like_buttons = document.querySelectorAll(".card__like-button");
+
 function openPopupEditProfile() {
-  popup.classList.add('popup__opened');
+  popup.classList.add("popup__opened");
   edit_profile_name.value = profile_name.textContent;
   edit_profile_about.value = profile_about.textContent;
 }
 
 function closePopupEditProfile() {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove("popup__opened");
 }
 
 function savePopupEditProfile(event) {
@@ -26,6 +28,13 @@ function savePopupEditProfile(event) {
   closePopupEditProfile();
 }
 
-edit_profile_open_button.addEventListener('click', openPopupEditProfile);
-edit_profile_close_button.addEventListener('click', closePopupEditProfile);
-edit_profile_save_button.addEventListener('click', savePopupEditProfile);
+edit_profile_open_button.addEventListener("click", openPopupEditProfile);
+edit_profile_close_button.addEventListener("click", closePopupEditProfile);
+edit_profile_save_button.addEventListener("click", savePopupEditProfile);
+
+for (let i = 0; i < card_like_buttons.length; i++) {
+  card_like_buttons[i].addEventListener("click", function() {
+    card_like_buttons[i].classList.toggle("card__like-button_active");
+  });
+}
+
