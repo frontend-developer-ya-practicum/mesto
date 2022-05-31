b15451e87426aec6f6332baa57f287025fb4f612
Александр Морев <a.morev@kryptonite.ru>
2022-05-31 10:00:05 +0300

    fix: add popup_opened modificator

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,20 @@ const editProfileOpenButton = profile.querySelector('.profile__edit-button');
 const profileName = profile.querySelector('.profile__name');
 const profileAbout = profile.querySelector('.profile__about');
 
-const editProfilePopup = document.querySelector('.popup_type_profile');
+const editProfilePopup = document.querySelector('.popup');
 const editProfileCloseButton = editProfilePopup.querySelector('.popup__close');
 const editProfileForm = editProfilePopup.querySelector('.popup__form')
 const editProfileName = editProfilePopup.querySelector('.popup__form-item_el_name');
 const editProfileAbout = editProfilePopup.querySelector('.popup__form-item_el_about');
 
 function openPopupEditProfile() {
-  editProfilePopup.classList.remove('popup_hidden');
+  editProfilePopup.classList.add('popup_opened');
   editProfileName.value = profileName.textContent;
   editProfileAbout.value = profileAbout.textContent;
 }
 
 function closePopupEditProfile() {
-  editProfilePopup.classList.add('popup_hidden');
+  editProfilePopup.classList.remove('popup_opened');
 }
 
 function savePopupEditProfile(event) {
