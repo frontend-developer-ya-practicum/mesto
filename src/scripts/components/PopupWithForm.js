069f6e7ee5cea25557f25b64c2173c438cee4851
Александр Морев <a.morev@kryptonite.ru>
2022-07-27 10:32:08 +0300

    Remove repeated element search in PopupWithForm

--- src/scripts/components/PopupWithForm.js
+++ src/scripts/components/PopupWithForm.js
@@ -4,8 +4,10 @@ class PopupWithForm extends Popup {
   constructor(popupSelector, handleFormSubmit) {
     super(popupSelector)
 
-    this._form = this._popup.querySelector('.popup__form');
     this._handleFormSubmit = handleFormSubmit;
+
+    this._form = this._popup.querySelector('.popup__form');
+    this._inputList = this._form.querySelectorAll('.popup__input');
   }
 
   close() {
@@ -24,8 +26,6 @@ class PopupWithForm extends Popup {
   }
 
   _getInputValues() {
-    this._inputList = this._form.querySelectorAll('.popup__input');
-
     this._formValues = {};
 
     this._inputList.forEach(input => {
